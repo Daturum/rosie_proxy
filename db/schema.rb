798fd2c9dc7283c7dc61692d7aff615f687d9dd4
Azamat Khudaygulov <afhudaygulov@daturum.ru>
2018-06-01 00:32:24 +0300

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_17_102737) do
+ActiveRecord::Schema.define(version: 2018_05_31_213056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "data_stores", force: :cascade do |t|
+    t.jsonb "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "type"
+  end
 
   create_table "rosie_asset_files", force: :cascade do |t|
     t.string "filename"
